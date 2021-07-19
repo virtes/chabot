a7f9aee623e2fff0db2a31adf6a1caeb66a17be7
virtes <2010pelmen2010@gmail.com>
2021-07-19 21:40:59 +0500

    Fix build

---
+++ src/Chabot.UnitTests/Fakes/Message.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Chabot.Messages;
+
+namespace Chabot.UnitTests.Fakes
+{
+    public class Message : IMessage
+    {
+        public string Id { get; set; }
+
+        public string RawText { get; set; }
+
+        public string SenderId { get; set; }
+
+        public IReadOnlyDictionary<string, string> Items { get; set; }
+    }
+}
