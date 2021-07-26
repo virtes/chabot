c803cfdaa3fd41b334365af63a515862cfaa0334
virtes <2010pelmen2010@gmail.com>
2021-07-26 22:19:52 +0500

    Fix build

---
+++ src/Chabot/Routing/IMessageRouter.cs
@@ -0,0 +1,11 @@
+﻿using Chabot.Commands;
+using Chabot.Messages;
+
+namespace Chabot.Routing
+{
+    public interface IMessageRouter<in TMessage>
+        where TMessage : IMessage
+    {
+        CommandInfo RouteMessage(TMessage message);
+    }
+}
