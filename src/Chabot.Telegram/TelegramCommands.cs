6fcbf778a22add5110c862158c0814f8a9235336
virtes <2010pelmen2010@gmail.com>
2025-02-20 23:30:52 +0500

    Add ChatId to TelegramCommands

--- src/Chabot.Telegram/TelegramCommands.cs
+++ src/Chabot.Telegram/TelegramCommands.cs
@@ -2,6 +2,7 @@ using Chabot.Commands;
 using Microsoft.Extensions.DependencyInjection;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace Chabot.Telegram;
 
@@ -9,4 +10,17 @@ public class TelegramCommands : CommandsBase<Update>
 {
     protected ITelegramBotClient BotClient
         => Context.ServiceProvider.GetRequiredService<ITelegramBotClientProvider>().GetClient();
+
+    protected long ChatId
+    {
+        get
+        {
+            return Context.Update.Type switch
+            {
+                UpdateType.Message => Context.Update.Message!.Chat.Id,
+                UpdateType.CallbackQuery => Context.Update.CallbackQuery!.Message!.Chat.Id,
+                _ => throw new InvalidOperationException("Could not resolve chat id")
+            };
+        }
+    }
 }
